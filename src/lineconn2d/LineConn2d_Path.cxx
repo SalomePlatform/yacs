ab796922af0b4bd92558dac6c100c97e76b7cd57
abd <abd@opencascade.com>
2008-11-28 10:55:52 +0000

    Update copyright information

--- src/lineconn2d/LineConn2d_Path.cxx
+++ src/lineconn2d/LineConn2d_Path.cxx
@@ -1,9 +1,26 @@
+//  Copyright (C) 2006-2008  CEA/DEN, EDF R&D, OPEN CASCADE
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+//  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
 // File:      LineConn2d_Path.cpp
 // Created:   11.08.05 15:16:13
 // Author:    Alexander GRIGORIEV
 // Copyright: Open Cascade 2005
-
-
+//
 #include <LineConn2dAfx.h>
 
 #include <LineConn2d_Path.h>
