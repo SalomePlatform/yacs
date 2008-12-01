25ad1a5b30f42b356c5d71b6377e25494a5cfae1
abd <abd@opencascade.com>
2008-12-01 10:36:35 +0000

    Update copyright information

--- src/engine/Plugin/fonction.hxx
+++ src/engine/Plugin/fonction.hxx
@@ -1,22 +1,21 @@
-// -*- C++ -*-
-// -*- coding: latin_1 -*-
+//  Copyright (C) 2006-2008  CEA/DEN, EDF R&D
 //
-//    File
-//      creation : 2007-02-26.04.24.28
-//      revision : $Id$
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License.
 //
-//    Copyright © 2007 Commissariat à l'Energie Atomique
-//      par Gilles ARNAUD (DM2S/SFME/LETR)
-//        C.E. Saclay; Bat 454; 91191 GIF/YVETTE CEDEX; France
-//        Tel: 01 69 08 38 86; Fax : 33 1 69 08 85 68 
-//        Gilles.Arnaud@cea.fr
-// 
-//    Object
-//      prototype de la fonction d evaluation
-// 
-//___________________________________________________________________
-
-
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+//  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
 #ifndef __FONCTION__
 #define __FONCTION__
 
