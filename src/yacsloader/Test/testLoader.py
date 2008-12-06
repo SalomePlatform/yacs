9bf49d161d65212f80315ead2df10f7e088e8e7c
prascle <prascle>
2008-12-06 11:48:08 +0000

    PR: adapt test after copyright

--- src/yacsloader/Test/testLoader.py
+++ src/yacsloader/Test/testLoader.py
@@ -57,7 +57,7 @@ class TestLoader(unittest.TestCase):
     except ValueError,ex:
       print "Caught ValueError Exception:",ex
       retex = ex
-    expected="LogRecord: parser:ERROR:from node node5 does not exist in control link: node5->b2 context: b1. (samples/bid.xml:33)\n"
+    expected="LogRecord: parser:ERROR:from node node5 does not exist in control link: node5->b2 context: b1. (samples/bid.xml:53)\n"
     self.assert_(p.getLogger("parser").getStr() == expected, "error not found")
     pass
 
