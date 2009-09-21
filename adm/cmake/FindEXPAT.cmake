708b1e2c8664d5ea48107a92486ec05f3fc9e703
adam <adam>
2009-09-21 08:54:22 +0000

    cmake port

---
+++ adm/cmake/FindEXPAT.cmake
@@ -0,0 +1,34 @@
+#  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
+#
+#  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+#  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
+IF(WINDOWS)
+  SET(EXPATHOME $ENV{EXPATHOME})
+  FIND_LIBRARY(EXPAT_LIB libexpat PATHS ${EXPATHOME}/bin ${EXPATHOME}/Bin)
+  FIND_PATH(EXPAT_INCLUDE_DIR expat.h PATHS ${EXPATHOME}/include ${EXPATHOME}/Source/lib)
+ELSE(WINDOWS)
+  FIND_LIBRARY(EXPAT_LIB expat)
+ENDIF(WINDOWS)
+
+IF(EXPAT_INCLUDE_DIR)
+  SET(EXPAT_INCLUDES -I${EXPAT_INCLUDE_DIR})
+ENDIF(EXPAT_INCLUDE_DIR)
+SET(EXPAT_LIBS ${EXPAT_LIB})
