SalomePlatform/yacs

commit dc812e72c1d3be07d20585778ef8aeaa185249f3

Fix a regression made by the HPContainer development in the GUI (commit 54dd4bca5d0eb605fae21321a06bbdee31dbe886).
Before the HPContainer development, when a node was added to the
schema from the catalog, a new instance of the component was created.
It was possible to change the instance, if desired.

After the HPContainer development, the same instance was used every
time the same service was added to the schema and there was no choice
to use different instances for the same service on different nodes.

This fix goes back to the behaviour before the HPContainer development.

src/hmi/commandsProc.cxx
@@ -242,7 +242,7 @@ bool CommandAddNodeFromCatalog::localExecute()
       ComposedNode* father =dynamic_cast<ComposedNode*> (node);
       if (father && nodeToClone)
         {
-          son = nodeToClone->cloneWithoutCompAndContDeepCpy(0);
+          son = nodeToClone->clone(0);
           son->setName(_name);
           service = dynamic_cast<ServiceNode*>(son);
         }
@@ -574,7 +574,7 @@ bool CommandPutInComposedNode::localExecute()
 	  //create a ComposedNode (type _type) with name _newParent
 	  YACS::ENGINE::Catalog *catalog = YACS::ENGINE::getSALOMERuntime()->getBuiltinCatalog();
 	  Node* nodeToClone = catalog->_composednodeMap[_type];
-	  composednode = nodeToClone->cloneWithoutCompAndContDeepCpy(0);
+	  composednode = nodeToClone->clone(0);
 	  composednode->setName(_newParent);
 	  //add the new composednode as child of oldfather
 	  oldFather->edAddChild(composednode);
@@ -709,7 +709,7 @@ bool CommandCopyNode::localExecute()
         if (!loop->edGetDirectDescendants().empty())
           throw YACS::Exception("Already a node in a new parent of Loop type");
       //_clone = node->cloneWithoutCompAndContDeepCpy(newFather);
-      _clone = node->cloneWithoutCompAndContDeepCpy(0);
+      _clone = node->clone(0);
       if (!_clone)
         throw YACS::Exception("Node cannot be cloned");
       int nodeSuffix = -1;

src/hmi/guiObservers.cxx
@@ -622,7 +622,7 @@ void SubjectNode::registerUndoDestroy()
   Bloc *undoBloc = new Bloc(blocName.str());
   undoProc->edAddChild(undoBloc);
   ComposedNode *newFather = undoBloc;
-  Node *clone = _node->cloneWithoutCompAndContDeepCpy(0);
+  Node *clone = _node->clone(0);
   newFather->edAddChild(clone);
 
   // --- register a CommandCopyNode from undoProc

commit 7a84ba3847602b1c8ba3991756fc5de55cfa530e

Fix a regression on copy/paste a node in schema.
The regression was introduced by commit dc812e72c1d3be07d20585778ef8aeaa185249f3.
There was an error message when a copy/paste was made on a node and it was impossible
to modify the pasted node.

src/hmi/commandsProc.cxx
@@ -574,7 +574,7 @@ bool CommandPutInComposedNode::localExecute()
 	  //create a ComposedNode (type _type) with name _newParent
 	  YACS::ENGINE::Catalog *catalog = YACS::ENGINE::getSALOMERuntime()->getBuiltinCatalog();
 	  Node* nodeToClone = catalog->_composednodeMap[_type];
-	  composednode = nodeToClone->clone(0);
+	  composednode = nodeToClone->cloneWithoutCompAndContDeepCpy(0);
 	  composednode->setName(_newParent);
 	  //add the new composednode as child of oldfather
 	  oldFather->edAddChild(composednode);
@@ -709,7 +709,7 @@ bool CommandCopyNode::localExecute()
         if (!loop->edGetDirectDescendants().empty())
           throw YACS::Exception("Already a node in a new parent of Loop type");
       //_clone = node->cloneWithoutCompAndContDeepCpy(newFather);
-      _clone = node->clone(0);
+      _clone = node->cloneWithoutCompAndContDeepCpy(0);
       if (!_clone)
         throw YACS::Exception("Node cannot be cloned");
       int nodeSuffix = -1;

src/hmi/guiObservers.cxx
@@ -622,7 +622,7 @@ void SubjectNode::registerUndoDestroy()
   Bloc *undoBloc = new Bloc(blocName.str());
   undoProc->edAddChild(undoBloc);
   ComposedNode *newFather = undoBloc;
-  Node *clone = _node->clone(0);
+  Node *clone = _node->cloneWithoutCompAndContDeepCpy(0);
   newFather->edAddChild(clone);
 
   // --- register a CommandCopyNode from undoProc