97f6e90e5168153d69e5b4a90f05878142a3b61d
rnv <rnv@opencascade.com>
2019-05-30 15:12:59 +0300

    Remove QT4 compatibility.

--- src/genericgui/GenericGui.cxx
+++ src/genericgui/GenericGui.cxx
@@ -174,9 +174,7 @@ GenericGui::GenericGui(YACS::HMI::SuitWrapper* wrapper, QMainWindow *parent)
   _parent->tabifyDockWidget(_dwStacked, _dwCatalogs);
   if (_wrapper->objectBrowser())
     _parent->tabifyDockWidget(_dwTree, _wrapper->objectBrowser());
-#if QT_VERSION >= 0x040500
   _parent->setTabPosition(Qt::AllDockWidgetAreas, Resource::tabPanelsUp? QTabWidget::North: QTabWidget::South);
-#endif
   //Import user catalog
   std::string usercata=Resource::userCatalog.toStdString();
   _catalogsWidget->addCatalogFromFile(usercata);
