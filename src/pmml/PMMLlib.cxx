e41dd6512d61d8270dec5ad62f4a0b6703ff112d
vsr <vsr@opencascade.com>
2022-05-05 17:16:48 +0300

    Copyright update 2022

--- src/pmml/PMMLlib.cxx
+++ src/pmml/PMMLlib.cxx
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2021 CEA/DEN
+// Copyright (C) 2013-2022 CEA/DEN
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published
